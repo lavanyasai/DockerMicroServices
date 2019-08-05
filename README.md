53e3581a5544c50b2ebe8f17964f202e5e6d2a5f
Lavanya Sai Pentela <lavanyasaipentela@gmail.com>
2019-08-05 17:27:22 +0530

    DockerizeMicroServices

---
+++ README.md
@@ -0,0 +1,3 @@
+Used Netflix Zuul as API Gateway.
+Used Eureka Server.
+Used Docker to create Images.
---
+++ zuul/src/main/java/com/example/gateway/ApiGatewayApplication.java
@@ -0,0 +1,32 @@
+package com.example.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public PreFilter preFilter() {
+		return new PreFilter();
+	}
+	@Bean
+	public PostFilter postFilter() {
+		return new PostFilter();
+	}
+	@Bean
+	public ErrorFilter errorFilter() {
+		return new ErrorFilter();
+	}
+	@Bean
+	public RouteFilter routeFilter() {
+		return new RouteFilter();
+	}
+}
